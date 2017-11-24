551247b88cd600f59c38bba13a30a4a304623860
digitopoi <hubo85@gmail.com>
2017-11-24 12:05:44 -0600

    Populate make dropdown with data from api

--- ClientApp/app/services/make.service.ts
+++ ClientApp/app/services/make.service.ts
@@ -8,6 +8,7 @@ export class MakeService {
   constructor(private http: Http) { }
 
   getMakes() {
-    return this.http.get('/api/makes');
+    return this.http.get('/api/makes')
+      .map(res => res.json());
   }
 }
