e2415f3daaff7be92bc6da9ad44aad02794009f9
digitopoi <hubo85@gmail.com>
2017-11-23 22:42:23 -0600

    Add makes controller

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        public  MakesConstroller(VegaDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await _context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
