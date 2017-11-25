b71e9e9b620573bdcbc0ea927cd146effe81dced
digitopoi <hubo85@gmail.com>
2017-11-24 22:39:06 -0600

    Add get vehicle by id functionality

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -81,5 +81,21 @@ namespace vega.Controllers
 
             return Ok(id);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleResource = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+        }
+
     }
 }
